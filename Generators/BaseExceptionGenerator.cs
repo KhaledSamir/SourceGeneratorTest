502cfffdb0bbafb9148a8d747ad9bd1050229ba2
Khaled <dev.KhaledSaleh@gmail.com>
2022-03-14 11:13:51 -0700

    Add files via upload

---
+++ Generators/BaseExceptionGenerator.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Diagnostics;
+
+namespace SoruceGenerator
+{
+    [Generator]
+    public class BaseExceptionGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+         //   Debugger.Launch();
+
+            foreach (var file in context.AdditionalFiles)
+            {
+                var jsSourceText = file.GetText();
+                var jsonText = jsSourceText?.ToString();
+               // var baseException = JsonSerializer.Deserialize<BaseException>(jsonText);
+            }
+            context.AddSource("BaseException", @"
+// This is generated file
+using System;
+
+namespace Test {
+    
+    public partial class Hello {
+    
+        public static void Display() {
+            
+            Console.WriteLine(""Hello world"");
+            
+        }
+    
+    }
+
+}
+");
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}
